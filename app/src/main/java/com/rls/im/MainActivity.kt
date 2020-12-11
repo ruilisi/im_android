199265991b6f599068c8322b53ff5044f9807fa6
xjd <771611622@qq.com>
2020-12-11 11:11:22 +0800

    add im config

--- app/src/main/java/com/rls/im/MainActivity.kt
+++ app/src/main/java/com/rls/im/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rls.im
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chat.android.im.config.IM
 import com.chat.android.im.config.UnifyDataConfig
@@ -21,12 +23,48 @@ import com.chat.android.im.helper.IMCallback
 ////    website = "https://github.com/novoda/${rootProject.name}"//github 地址
 //        }
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var server: EditText
+    private lateinit var roomId: EditText
+    private lateinit var userId: EditText
+    private lateinit var userToken: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        server = findViewById(R.id.server)
+        roomId = findViewById(R.id.room_id)
+        userId = findViewById(R.id.user_id)
+        userToken = findViewById(R.id.user_token)
     }
 
     fun connect(view: View?) {
+
+        if (server.text.trim().isEmpty()) {
+            showToas("server can not be empty")
+            return
+        }
+
+        if (roomId.text.trim().isEmpty()) {
+            showToas("room id can not be empty")
+            return
+        }
+
+        if (userId.text.trim().isEmpty()) {
+            showToas("user id can not be empty")
+            return
+        }
+
+        if (userToken.text.trim().isEmpty()) {
+            showToas("user token can not be empty")
+            return
+        }
+
+
         val im = IM.getInstance(application)
         im.setDataConfig(dataConfig)
         im.openChat(object : IMCallback {
@@ -37,9 +75,15 @@ class MainActivity : AppCompatActivity() {
 
     private val dataConfig: UnifyDataConfig
         private get() = UnifyDataConfig.Builder()
-                .setUrl("wss://chat.esheeps.com/websocket")
-                .setRoomId("ZQTjpYHzu8etK5s5t")
-                .setUserId("2i984iAtbfPpYA78X")
-                .setUserToken("2KWPRX_fZsCKHMY8duT9VigBleXR8o52Jsa130fNvll")
+                .setUrl(server.text.toString())
+                .setRoomId(roomId.text.toString())
+                .setUserId(userId.text.toString())
+                .setUserToken(userToken.text.toString())
                 .build()
+
+
+    private fun showToas(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+    }
 }
